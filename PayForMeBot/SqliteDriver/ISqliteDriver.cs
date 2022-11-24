0133fdcb5d00a4f27fc2f9ef8f804843cc2e1fd2
Ivan Sychev <funovichf@gmail.com>
2022-11-24 17:22:23 +0500

    add test db interface

---
+++ PayForMeBot/SqliteDriver/ISqliteDriver.cs
@@ -0,0 +1,13 @@
+namespace PayForMeBot.SqliteDriver;
+
+public interface ISqliteDriver
+{
+    // Таблица пользователей
+    // Таблица продуктов
+    // Таблица сопоставления продуктов и пользователей
+
+    void AddUser(string telegramId, Guid teamId); // sbpLink ???
+    void AddProduct(object product, Guid teamId, Guid receiptId, string buyerTelegramId); // product = id, name, price
+    void AddProducts(object[] products, Guid teamId, Guid receiptId, string buyerTelegramId);
+    void AddUserProductBinding(string userTelegramId, Guid productId, Guid teamId);
+}
