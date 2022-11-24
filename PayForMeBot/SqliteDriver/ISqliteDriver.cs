3718809f405105189a7c6f15ae8fe0e49daac522
Ivan Sychev <funovichf@gmail.com>
2022-11-24 19:58:48 +0500

    small sqlite driver interface changes

--- PayForMeBot/SqliteDriver/ISqliteDriver.cs
+++ PayForMeBot/SqliteDriver/ISqliteDriver.cs
@@ -6,7 +6,8 @@ public interface ISqliteDriver
     // Таблица продуктов
     // Таблица сопоставления продуктов и пользователей
 
-    void AddUser(string telegramId, Guid teamId); // sbpLink ???
+    void AddUser(string telegramId, Guid teamId);
+    void AddSbpLink(string sbpLink);
     void AddProduct(object product, Guid teamId, Guid receiptId, string buyerTelegramId); // product = id, name, price
     void AddProducts(object[] products, Guid teamId, Guid receiptId, string buyerTelegramId);
     void AddUserProductBinding(string userTelegramId, Guid productId, Guid teamId);
