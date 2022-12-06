d35f4b9b70ad491f209b96bfd1e5f25628a558ba
Ivan Sychev <funovichf@gmail.com>
2022-12-07 00:44:52 +0500

    add break sqlite db

--- PayForMeBot/SqliteDriver/DbContext.cs
+++ PayForMeBot/SqliteDriver/DbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using PayForMeBot.ReceiptApiClient.Models;
 using PayForMeBot.SqliteDriver.Models;
 using Product = PayForMeBot.SqliteDriver.Models;
 
@@ -7,17 +6,24 @@ namespace PayForMeBot.SqliteDriver;
 
 public sealed class DbContext : Microsoft.EntityFrameworkCore.DbContext
 {
-    public DbSet<UserTable> Users { get; set; }
-    public DbSet<ProductTable> Products { get; set; }    
-    public DbSet<UserProductTable> Bindings { get; set; }
+    public DbSet<UserTable>? Users { get; set; }
+    public DbSet<ProductTable>? Products { get; set; }    
+    public DbSet<UserProductTable>? Bindings { get; set; }
+    
+    public string DbPath { get; }
 
     public DbContext()
     {
-        Database.EnsureCreated();
+        var folder = Environment.CurrentDirectory;
+        // Environment.SpecialFolder.LocalApplicationData;
+        // var path = Environment.GetFolderPath();
+        DbPath = Path.Join(folder, "UsersAndProducts.db");
+        // Database.EnsureCreated();
     }
  
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=UsersAndProducts;Trusted_Connection=True;");
+        optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        // optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=UsersAndProducts;Trusted_Connection=True;");
     }
 }
