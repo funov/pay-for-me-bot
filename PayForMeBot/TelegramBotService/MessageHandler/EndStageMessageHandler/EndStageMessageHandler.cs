aea55761c0e2df9ec1d9f49a8b3e26a289d28920
Ivan Sychev <funovichf@gmail.com>
2022-12-12 23:38:21 +0500

    fix final

--- PayForMeBot/TelegramBotService/MessageHandler/EndStageMessageHandler/EndStageMessageHandler.cs
+++ PayForMeBot/TelegramBotService/MessageHandler/EndStageMessageHandler/EndStageMessageHandler.cs
@@ -66,12 +66,13 @@ public class EndStageMessageHandler : IEndStageMessageHandler
                         await SendRequisitesAndDebts(client, teamChatId, cancellationToken,
                             teamUsers2Buyers2Money[teamChatId]);
 
+                        dbDriver.ChangeUserStage(chatId, teamId, "start");
+
                         await client.SendTextMessageAsync(
-                            chatId: chatId,
+                            chatId: teamChatId,
                             text: "Создай или присоединись к команде!",
                             replyMarkup: keyboardMarkup.GetReplyKeyboardMarkup(teamSelectionLabels),
                             cancellationToken: cancellationToken);
-                        dbDriver.ChangeUserStage(chatId, teamId, "start");
                     }
 
                     dbDriver.DeleteTeamInDb(teamId);
